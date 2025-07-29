3599a555b08becf3f23d3a5eb520e5b67d221ea4
Ketan Bajpai <149509716+ketaniiitn@users.noreply.github.com>
2025-07-29 22:41:20 +0530

    Update app.py for docker-image

--- backend/app.py
+++ backend/app.py
@@ -79,4 +79,4 @@ app.register_blueprint(product_bp, url_prefix='/products')
 
 # --- Run App ---
 if __name__ == '__main__':
- app.run(debug=True, port=8080)
+ app.run(debug=True, host='0.0.0.0', port=8080)
